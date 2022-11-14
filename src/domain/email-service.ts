e7fed7ed28b18f90ee5a3a7ce48b4cc3b60fe46a
Dronchik23 <108690351+Dronchik23@users.noreply.github.com>
2022-11-14 21:56:59 +0300

    +

--- src/domain/email-service.ts
+++ src/domain/email-service.ts
@@ -2,12 +2,9 @@ import {emailAdapter} from "../adapters/email-adapter";
 
 export const emailService = {
     async sendEmail(email: any, subject: string, message: string) {
-        await emailAdapter.sendEmail('user.email', 'some subject', '<div>message</div>')
+        await emailAdapter.sendEmail(email, subject, message)
     },
     async sendEmailConfirmationMessage(user: any) {
-        // save to repo
-        // get user from repo
-        await emailAdapter.sendEmail('user.email', 'password recovery', '<div>${user.confirmationCode}message</div>')
+        await emailAdapter.sendEmail(user.accountData.email, 'some subject', `<div>${user.confirmationCode}message</div>`)
     }
-
 }
