ecdd5e368c081e82a5c80be92367161888c29cc2
Dronchik23 <108690351+Dronchik23@users.noreply.github.com>
2022-11-15 22:54:55 +0300

    +

--- src/domain/email-service.ts
+++ src/domain/email-service.ts
@@ -7,10 +7,14 @@ export const emailService = {
     },
     async sendEmailConfirmationMessage(user: UserDBType) {
         const code = user.emailConfirmation.confirmationCode
-        await emailAdapter.sendEmail(user.accountData.email,
-            'Confirm your email' , `<a href='http://localhost:2000/auth/registration-confirmation?code=${code}'>complete registration</a>`)
+        await emailAdapter.sendEmail(
+            user.accountData.email, 'Confirm your email' ,
+            `<a 
+href='http://localhost:2000/auth/registration-confirmation?code=<${code}>'>complete registration</a>`
+        )
     }
 }
 
 
   ///  <a href='${config.linkBase}/auth/confirm-email?code=${code}'>complete registration</a>
+
