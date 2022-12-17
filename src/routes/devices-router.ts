7f0dc0bfe9c3957e1eb634eed06cbd08d5070917
Dronchik23 <108690351+Dronchik23@users.noreply.github.com>
2022-12-17 23:41:53 +0300

    fix

--- src/routes/devices-router.ts
+++ src/routes/devices-router.ts
@@ -1,17 +1,23 @@
 import {container} from "../composition-root";
 import {DevicesController} from "../controller/devices-controller";
 import {Router} from "express";
-import {authJWTMiddleware} from "../middlewares/bearer-auth-miidleware";
+import rateLimit from 'express-rate-limit'
+
+
+const limiter = rateLimit({
+    windowMs: 10000,
+    max: 5
+})
 
 
 const devicesController = container.resolve(DevicesController)
 
 export const devicesRouter = Router({})
 
-devicesRouter.get('/devices', devicesController.getAllDevices.bind(devicesController))
+devicesRouter.get('/devices', limiter, devicesController.getAllDevices.bind(devicesController))
 
-devicesRouter.delete('/devices', devicesController.deleteAllDevicesExcludeCurrent.bind(devicesController))
+devicesRouter.delete('/devices', limiter, devicesController.deleteAllDevicesExcludeCurrent.bind(devicesController))
 
-devicesRouter.delete('/devices/:deviceId', devicesController
+devicesRouter.delete('/devices/:deviceId', limiter, devicesController
     .deleteDeviceByDeviceId.bind(devicesController))
 
